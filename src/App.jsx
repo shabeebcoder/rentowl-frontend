a7301c14b4a88b329b3476cb0b0cc1ccc19ac9db
JunaidParamberi <145155780+JunaidParamberi@users.noreply.github.com>
2024-04-21 22:34:24 +0400

    react router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import './App.css'
 import Signup from './Pages/Signup'
+import { Routes, Route } from "react-router-dom";
+import SignInputs from './Components/SignInputs';
+import LoginInputs from './Components/LoginInputs';
+import UserInfo from './Components/UserInfo';
 
 function App() {
 
 
   return (
     <>
+    <Routes>
+      <Route path='/' element = {<Signup/>} >
+          <Route index element = {<SignInputs/>} />
+          <Route path='login' element = {<LoginInputs/>} />
+          <Route path='user_info' element = {<UserInfo/>} />
+      </Route>
+    </Routes>
     <Signup/>
     </>
   )
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -3,6 +3,7 @@ import SignInputs from '../Components/SignInputs'
 import LoginInputs from '../Components/LoginInputs'
 import UserInfo from '../Components/UserInfo'
 import cover from '../assets/BG-1.jpeg'
+import { Outlet } from 'react-router-dom'
 
 
 function Signup() {
@@ -12,10 +13,11 @@ function Signup() {
 
         <div className="input-container bg-white
         w-[45%] flex flex-col  items-center h-[100vh] overflow-hidden">
+          <Outlet/>
 
         {/* <SignInputs/> */}
         {/* <LoginInputs/> */}
-        <UserInfo/>
+        {/* <UserInfo/> */}
         </div>
 
   
